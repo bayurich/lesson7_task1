68ff406c37d06940e0f4ddd6ebde76ee33497f7c
Aleks <beilis@mail.ru>
2022-01-09 21:41:29 +0500

    add tests (TDD)

---
+++ src/main/java/ru/netology/Calculator.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import java.math.BigDecimal;
+import java.time.Month;
+
+public class Calculator {
+
+    public BigDecimal calcMonthPayment(BigDecimal sum, int period, BigDecimal rate, int days){
+
+        return BigDecimal.ZERO;
+    }
+
+    public BigDecimal calcFullSum(BigDecimal sum, int period, BigDecimal rate, Month month){
+
+        return BigDecimal.ZERO;
+    }
+
+    public BigDecimal calcOverpayment(BigDecimal sum, BigDecimal fullSum){
+
+        return BigDecimal.ZERO;
+    }
+}
+
---
+++ src/test/java/ru/netology/test/TestCalculator.java
@@ -0,0 +1,49 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.Calculator;
+
+import java.math.BigDecimal;
+import java.time.Month;
+
+public class TestCalculator {
+
+    @Test
+    public void testMonthPayment() {
+        Calculator calculator = new Calculator();
+        BigDecimal sum = new BigDecimal("1000000");
+        int period = 60;
+        BigDecimal rate = new BigDecimal("8.5");
+        int days = 30;
+        BigDecimal sumExpectResult = new BigDecimal("23652.97");
+
+        BigDecimal sumCalcResult = calculator.calcMonthPayment(sum, period, rate, days);
+        Assertions.assertTrue(sumExpectResult.compareTo(sumCalcResult) == 0);
+    }
+
+    @Test
+    public void testFullSum() {
+        Calculator calculator = new Calculator();
+        BigDecimal sum = new BigDecimal("1000000");
+        int period = 60;
+        BigDecimal rate = new BigDecimal("8.5");
+        BigDecimal sumExpectResult = new BigDecimal("1215857.27");
+
+        BigDecimal sumCalcResult = calculator.calcFullSum(sum, period, rate, Month.JANUARY);
+        Assertions.assertTrue(sumExpectResult.compareTo(sumCalcResult) == 0);
+    }
+
+    @Test
+    public void testSumOverpayment() {
+        Calculator calculator = new Calculator();
+        BigDecimal sum = new BigDecimal("1000000");
+        int period = 60;
+        BigDecimal rate = new BigDecimal("8.5");
+        BigDecimal sumExpectResult = new BigDecimal("215857.27");
+
+        BigDecimal sumCalcResult = calculator.calcOverpayment(sum, calculator.calcFullSum(sum, period, rate, Month.JANUARY));
+        System.out.println("sumCalcResult: " + sumCalcResult);
+        Assertions.assertTrue(sumExpectResult.compareTo(sumCalcResult) == 0);
+    }
+}
